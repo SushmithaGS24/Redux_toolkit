b4a21db94a1eec95b061294b4ab21271ce431a88
Sushmitha   G S <80742062+SushmithaGS24@users.noreply.github.com>
2025-02-10 14:13:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Counter from './features/counters/counter.js';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Counter />
+    
+    </div>
+  );
+}
+
+export default App;
